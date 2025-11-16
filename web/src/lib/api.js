a03d1943174a8ec6355419a597da0533b701a3c7
Joy Biswas <joybiswas040701@gmail.com>
2025-11-16 17:05:02 +0600

    refactor: use relative api paths for production builds

--- web/src/lib/api.js
+++ web/src/lib/api.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-const { VITE_BASE_URL: baseURL } = import.meta.env;
-
 const api = axios.create({
-  baseURL: `${baseURL}/api/v1`,
+  baseURL: "/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
--- web/vite.config.js
+++ web/vite.config.js
@@ -3,12 +3,27 @@ import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import tailwindcss from "@tailwindcss/vite";
 
+let devProxyServer = "http://localhost:8000";
+if (process.env.DEV_PROXY_SERVER && process.env.DEV_PROXY_SERVER.length > 0) {
+  console.log(
+    "Use devProxyServer from environment: ",
+    process.env.DEV_PROXY_SERVER,
+  );
+  devProxyServer = process.env.DEV_PROXY_SERVER;
+}
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: "0.0.0.0",
     port: 3001,
+    proxy: {
+      "^/api": {
+        target: devProxyServer,
+        xfwd: true,
+      },
+    },
   },
   resolve: {
     alias: {
